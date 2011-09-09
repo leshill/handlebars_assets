d3c873efd256328e7e6a6f2b3f95463b88a5a31f
Matt Burke <spraints@gmail.com>
2011-09-08 20:42:32 -0400

    Add a test.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source "http://rubygems.org"
 
+gem 'rake'
+gem 'activesupport', '~>3.1.0'
+
 # Specify your gem's dependencies in handlebars_assets.gemspec
 gemspec
---
+++ test/handlebars_assets/tilt_handlebars_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+module HandlebarsAssets
+  class TiltHandlebarsTest < Test::Unit::TestCase
+    def test_render
+      # Try to act like sprockets.
+      scope = Object.new
+      class << scope
+        def logical_path ; 'x11' ; end
+      end
+      template = HandlebarsAssets::TiltHandlebars.new('/myapp/app/assets/templates/x11.jst.hbs') { "This is {{handlebars}}" }
+      assert_equal <<END_EXPECTED, template.render(scope, {})
+          function(context) {
+            return HandlebarsTemplates["x11"](context);
+          };
+          this.HandlebarsTemplates || (this.HandlebarsTemplates = {});
+          this.HandlebarsTemplates["x11"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  helpers = helpers || Handlebars.helpers;
+  var buffer = "", stack1, self=this, functionType="function", helperMissing=helpers.helperMissing, undef=void 0, escapeExpression=this.escapeExpression;
+
+
+  buffer += "This is ";
+  stack1 = helpers.handlebars || depth0.handlebars
+  if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
+  else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "handlebars", { hash: {} }); }
+  buffer += escapeExpression(stack1);
+  return buffer;});
+END_EXPECTED
+    end
+  end
+end
