b77bb97b1920680fca17db4311fbcec4f11f0a51
Thorben Schröder <thorben.schroeder@gmail.com>
2011-10-05 20:52:11 +0300

    Makes the engine work with Rails 3.1.1.rc2's way of asset pre compilation

--- lib/handlebars_assets/engine.rb
+++ lib/handlebars_assets/engine.rb
@@ -1,6 +1,6 @@
 module HandlebarsAssets
   class Engine < ::Rails::Engine
-    initializer "sprockets.handlebars", :after => "sprockets.environment" do |app|
+    initializer "sprockets.handlebars", :after => "sprockets.environment", :group => :assets do |app|
       next unless app.assets
       app.assets.register_engine('.hbs', TiltHandlebars)
     end
