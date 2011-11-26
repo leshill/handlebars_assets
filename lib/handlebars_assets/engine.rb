25c565a2be64a54cfceb2815d287381f4dcaa6ff
Les Hill <leshill@gmail.com>
2011-11-26 12:50:54 -0800

    Use group :all
    - allows for precompiling and on-the-fly (dev usually)

--- lib/handlebars_assets/engine.rb
+++ lib/handlebars_assets/engine.rb
@@ -1,6 +1,6 @@
 module HandlebarsAssets
   class Engine < ::Rails::Engine
-    initializer "sprockets.handlebars", :after => "sprockets.environment", :group => :assets do |app|
+    initializer "sprockets.handlebars", :after => "sprockets.environment", :group => :all do |app|
       next unless app.assets
       app.assets.register_engine('.hbs', TiltHandlebars)
     end
