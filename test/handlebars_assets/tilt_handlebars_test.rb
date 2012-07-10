e0ae8c406aa274b9063f07b84a9062b4d5923f3b
Les Hill <leshill@gmail.com>
2012-07-10 14:45:33 -0700

    Fix specs

--- test/handlebars_assets/tilt_handlebars_test.rb
+++ test/handlebars_assets/tilt_handlebars_test.rb
@@ -14,11 +14,12 @@ module HandlebarsAssets
             this.HandlebarsTemplates || (this.HandlebarsTemplates = {});
             this.HandlebarsTemplates["x11"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   helpers = helpers || Handlebars.helpers;
-  var buffer = "", stack1, self=this, functionType="function", helperMissing=helpers.helperMissing, undef=void 0, escapeExpression=this.escapeExpression;
+  var buffer = "", stack1, foundHelper, self=this, functionType="function", helperMissing=helpers.helperMissing, undef=void 0, escapeExpression=this.escapeExpression;
 
 
   buffer += "This is ";
-  stack1 = helpers.handlebars || depth0.handlebars;
+  foundHelper = helpers.handlebars;
+  stack1 = foundHelper || depth0.handlebars;
   if(typeof stack1 === functionType) { stack1 = stack1.call(depth0, { hash: {} }); }
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "handlebars", { hash: {} }); }
   buffer += escapeExpression(stack1);
