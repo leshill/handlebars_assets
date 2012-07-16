d09ff0e34ae723d1127f574d921ff170a418d9de
Les Hill <leshill@gmail.com>
2012-07-15 22:41:35 -0700

    Refactor test

--- test/handlebars_assets/tilt_handlebars_test.rb
+++ test/handlebars_assets/tilt_handlebars_test.rb
@@ -13,11 +13,11 @@ module HandlebarsAssets
       end.new
     end
 
-    def hbs_compiled
+    def hbs_compiled(template_name)
       <<END_EXPECTED
           (function() {
             this.HandlebarsTemplates || (this.HandlebarsTemplates = {});
-            this.HandlebarsTemplates["x11"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+            this.HandlebarsTemplates["#{template_name}"] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
   helpers = helpers || Handlebars.helpers;
   var buffer = "", stack1, foundHelper, self=this, functionType="function", helperMissing=helpers.helperMissing, undef=void 0, escapeExpression=this.escapeExpression;
 
@@ -29,31 +29,31 @@ module HandlebarsAssets
   else if(stack1=== undef) { stack1 = helperMissing.call(depth0, "handlebars", { hash: {} }); }
   buffer += escapeExpression(stack1);
   return buffer;});
-            return HandlebarsTemplates["x11"];
+            return HandlebarsTemplates["#{template_name}"];
           }).call(this);
 END_EXPECTED
     end
 
     def test_render
       root = '/myapp/app/assets/templates'
-      file = 'x11.hbs'
+      file = 'test_render.hbs'
       scope = make_scope root, file
 
       template = HandlebarsAssets::TiltHandlebars.new(scope.pathname.to_s) { "This is {{handlebars}}" }
 
-      assert_equal hbs_compiled, template.render(scope, {})
+      assert_equal hbs_compiled('test_render'), template.render(scope, {})
     end
 
     # Sprockets does not add nested root paths (i.e.
     # app/assets/javascripts/templates is rooted at app/assets/javascripts)
     def test_template_misnaming
       root = '/myapp/app/assets/javascripts'
-      file = 'templates/x11.hbs'
+      file = 'templates/test_template_misnaming.hbs'
       scope = make_scope root, file
 
       template = HandlebarsAssets::TiltHandlebars.new(scope.pathname.to_s) { "This is {{handlebars}}" }
 
-      assert_equal hbs_compiled, template.render(scope, {})
+      assert_equal hbs_compiled('test_template_misnaming'), template.render(scope, {})
     end
   end
 end
