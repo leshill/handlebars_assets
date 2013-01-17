f09df882740f48790dd06ac5ea5a1a4c6c96d85b
Deepak Kannan <kannan.deepak@gmail.com>
2013-01-17 11:26:24 +0530

    render templates server side for rails
    using ExecJS and the handlebars javascript source
    the handlebars templates is rendered on the server
    
    objective is to keep templates DRY. same template
    on the client and server

--- lib/handlebars_assets/engine.rb
+++ lib/handlebars_assets/engine.rb
@@ -1,3 +1,5 @@
+require 'action_view/template/handlers/hbs'
+
 module HandlebarsAssets
   class Engine < ::Rails::Engine
     initializer "sprockets.handlebars", :after => "sprockets.environment", :group => :all do |app|
@@ -6,5 +8,14 @@ module HandlebarsAssets
       app.assets.register_engine('.handlebars', TiltHandlebars)
       app.assets.register_engine('.hamlbars', TiltHandlebars) if HandlebarsAssets::Config.haml_available?
     end
+
+    initializer "handlebars.register_template_handler" do
+      ActiveSupport.on_load(:action_view) do
+        [:hbs, :handlebars].each do |ext|
+          ActionView::Template.register_template_handler(ext, ActionView::Template::Handlers::HBS)
+        end
+      end
+    end
+    
   end
 end
