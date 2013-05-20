2b1644df2aa27294b0182ab623478615828c420c
Turadg Aleahmad <turadg@aleahmad.net>
2013-05-19 22:50:24 -0400

    fix failing #test_patching_handlebars

--- test/patch/patch.js
+++ test/patch/patch.js
@@ -1,12 +1,10 @@
-(function(window) {
-  var originalLookup = window.Handlebars.JavaScriptCompiler.prototype.nameLookup;
+var originalLookup = Handlebars.JavaScriptCompiler.prototype.nameLookup;
 
-  window.Handlebars.JavaScriptCompiler.prototype.nameLookup = function(parent, name, type) {
-    if (type === 'context') {
-      return '"CALLED PATCH"';
-    }
-    else {
-      return originalLookup.call(this, parent, name, type);
-    }
-  };
-})(this);
+Handlebars.JavaScriptCompiler.prototype.nameLookup = function(parent, name, type) {
+  if (type === 'context') {
+    return '"CALLED PATCH"';
+  }
+  else {
+    return originalLookup.call(this, parent, name, type);
+  }
+};
