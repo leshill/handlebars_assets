b1d7bea77e87539ed9341a6101bc536b2928653e
Jose Peleteiro <jose@peleteiro.net>
2013-06-19 21:39:57 -0300

    Register sprockets engine in config.before_initialize. Making it rails 4 compatible

--- lib/handlebars_assets/engine.rb
+++ lib/handlebars_assets/engine.rb
@@ -1,11 +1,13 @@
 module HandlebarsAssets
   class Engine < ::Rails::Engine
-    initializer "sprockets.handlebars", :after => "sprockets.environment", :group => :all do |app|
-      next unless app.assets
+
+    config.before_initialize do |app|
+      Sprockets::Engines #force autoloading
       Sprockets.register_engine('.hbs', TiltHandlebars)
       Sprockets.register_engine('.handlebars', TiltHandlebars)
       Sprockets.register_engine('.hamlbars', TiltHandlebars) if HandlebarsAssets::Config.haml_available?
       Sprockets.register_engine('.slimbars', TiltHandlebars) if HandlebarsAssets::Config.slim_available?
     end
+
   end
 end
