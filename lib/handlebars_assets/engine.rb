cf0066cc06e7ee0d5ff28c09d4f7aa4830c7bf7f
Les Hill <leshill@gmail.com>
2013-06-21 09:36:44 -0700

    Revert "Resister engine using `Sprockets` vs `app.assets`."
    Not convinced that this is the 'right thing to do'.
    
    This reverts commit c1cea5dc60c3806ca1d45ba16b38457fc748ffc1.

--- lib/handlebars_assets/engine.rb
+++ lib/handlebars_assets/engine.rb
@@ -2,10 +2,10 @@ module HandlebarsAssets
   class Engine < ::Rails::Engine
     initializer "sprockets.handlebars", :after => "sprockets.environment", :group => :all do |app|
       next unless app.assets
-      Sprockets.register_engine('.hbs', TiltHandlebars)
-      Sprockets.register_engine('.handlebars', TiltHandlebars)
-      Sprockets.register_engine('.hamlbars', TiltHandlebars) if HandlebarsAssets::Config.haml_available?
-      Sprockets.register_engine('.slimbars', TiltHandlebars) if HandlebarsAssets::Config.slim_available?
+      app.assets.register_engine('.hbs', TiltHandlebars)
+      app.assets.register_engine('.handlebars', TiltHandlebars)
+      app.assets.register_engine('.hamlbars', TiltHandlebars) if HandlebarsAssets::Config.haml_available?
+      app.assets.register_engine('.slimbars', TiltHandlebars) if HandlebarsAssets::Config.slim_available?
     end
   end
 end
