84a09492f5067f96b147acaadeda2f424a5774b3
Alex Riedler <alex@riedler.ca>
2013-09-20 21:34:27 -0700

    step 1, refactor

--- lib/handlebars_assets.rb
+++ lib/handlebars_assets.rb
@@ -1,23 +1,27 @@
-require "handlebars_assets/version"
+require 'handlebars_assets/version'
+require 'sprockets'
 
 module HandlebarsAssets
-  PATH = File.expand_path("../../vendor/assets/javascripts", __FILE__)
+  PATH = File.expand_path('../../vendor/assets/javascripts', __FILE__)
 
   def self.path
     PATH
   end
 
+  def register_extensions(sprockets_environment)
+      sprockets_environment.register_engine('.hbs', TiltHandlebars)
+      sprockets_environment.register_engine('.handlebars', TiltHandlebars)
+      sprockets_environment.register_engine('.hamlbars', TiltHandlebars) if HandlebarsAssets::Config.haml_available?
+      sprockets_environment.register_engine('.slimbars', TiltHandlebars) if HandlebarsAssets::Config.slim_available?
+  end
+
   autoload(:Config, 'handlebars_assets/config')
   autoload(:Handlebars, 'handlebars_assets/handlebars')
   autoload(:TiltHandlebars, 'handlebars_assets/tilt_handlebars')
+end
 
-  if defined?(Rails) && defined?(::Rails::Engine)
-    require 'handlebars_assets/engine'
-  else
-    require 'sprockets'
-    Sprockets.register_engine '.hbs', TiltHandlebars
-    Sprockets.register_engine '.handlebars', TiltHandlebars
-    Sprockets.register_engine('.hamlbars', TiltHandlebars) if HandlebarsAssets::Config.haml_available?
-    Sprockets.register_engine('.slimbars', TiltHandlebars) if HandlebarsAssets::Config.slim_available?
-  end
+if defined?(Rails)
+  require 'handlebar_assets/railtie'
+else
+  register_extensions(Sprockets)
 end
