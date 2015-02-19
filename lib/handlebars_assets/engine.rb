dcf314712b9bebe5e2e14dc9f12da387d66810d8
Alex Riedler <ariedler@codified.co>
2015-02-18 21:06:07 -0500

    should be asset version.

--- lib/handlebars_assets/engine.rb
+++ lib/handlebars_assets/engine.rb
@@ -2,8 +2,8 @@ module HandlebarsAssets
   # NOTE: must be an engine because we are including assets in the gem
   class Engine < ::Rails::Engine
     initializer "handlebars_assets.assets.register", :group => :all do |app|
-      ::HandlebarsAssets::register_extensions(Sprockets)
-      ::HandlebarsAssets::add_to_asset_versioning(Sprockets)
+      ::HandlebarsAssets::register_extensions(app.assets)
+      app.assets.version += "#{::HandlebarsAssets::VERSION}"
     end
   end
 end
