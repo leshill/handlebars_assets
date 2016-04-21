70a738c2011ff793a78980dec4cc180ac2cdad3c
Michael Lawlor <mike@auka.ca>
2016-04-21 16:50:37 -0700

    Fixes a bug that was preventing Rails app startup
    Error was:
    
    ```
    'method_missing': undefined method `assets' for #<Rails::Engine::Configuration:0x007fca48b60e58> (NoMethodError)
    from handlebars_assets/lib/handlebars_assets/engine.rb:5:in 'block in <class:Engine>'
    ```

--- lib/handlebars_assets/engine.rb
+++ lib/handlebars_assets/engine.rb
@@ -2,7 +2,7 @@ module HandlebarsAssets
   # NOTE: must be an engine because we are including assets in the gem
   class Engine < ::Rails::Engine
     initializer "handlebars_assets.assets.register", :group => :all do |app|
-      config.assets.configure do |sprockets_env|
+      app.config.assets.configure do |sprockets_env|
         ::HandlebarsAssets::register_extensions(sprockets_env)
         if Gem::Version.new(Sprockets::VERSION) < Gem::Version.new('3')
           ::HandlebarsAssets::add_to_asset_versioning(sprockets_env)
